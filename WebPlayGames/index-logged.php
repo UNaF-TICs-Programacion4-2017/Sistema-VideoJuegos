4a00bbba5cfa48f283c56a10ad9500549e7eece7
MSI\ezequ <ezequielbogadozenith@gmail.com>
2017-06-16 00:39:05 -0300

    Cambios de HTML a PHP y Cambio en interfaz

--- WebPlayGames/index-logged.php
+++ WebPlayGames/index-logged.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 		
-		<title>PLAY GAMES FORMOSA</title>
+		<title>Ecommerce Video Game</title>
 
 		<!-- Loading third party fonts -->
 		<link href="http://fonts.googleapis.com/css?family=Roboto:100,300,400,700|" rel="stylesheet" type="text/css">
@@ -28,32 +28,34 @@
 		<div id="site-content">
 			<div class="site-header">
 				<div class="container">
-					<a href="index.html" id="branding">
-						<img src="images/logo5.png" alt="" class="logo">
+					<a href="index.php" id="branding">
+						<img src="images/logo.png" alt="" class="logo">
 						<div class="logo-text">
-							<h1 class="site-title">PLAY GAMES</h1>
-							<small class="site-description">Venta de Videojuegos y Accesorios</small>
+							<h1 class="site-title">Company name</h1>
+							<small class="site-description">Tagline goes here</small>
 						</div>
 					</a> <!-- #branding -->
 
 					<div class="right-section pull-right">
-						<a href="cart.html" class="cart"><i class="icon-cart"></i> 0 items in cart</a>
-						<a href="#" class="login-button">Login/Register</a>
+						<a href="cart.php" class="cart"><i class="icon-cart"></i> 0 items in cart</a>
+						<a href="#">My Account</a>
+						<a href="#">Logout <small>(John Smith)</small></a>
 					</div> <!-- .right-section -->
 
 					<div class="main-navigation">
 						<button class="toggle-menu"><i class="fa fa-bars"></i></button>
 						<ul class="menu">
-							<li class="menu-item home current-menu-item"><a href="index.html"><i class="icon-home"></i></a></li>
-							<li class="menu-item"><a href="products.html">Accesorios</a></li>
-							<li class="menu-item"><a href="products.html">Promociones</a></li>
-							<li class="menu-item"><a href="products.html">Playstation</a></li>
-							<li class="menu-item"><a href="products.html">Xbox</a></li>
-							<li class="menu-item"><a href="products.html">Nintendo</a></li>
+							<li class="menu-item home current-menu-item"><a href="index.php"><i class="icon-home"></i></a></li>
+							<li class="menu-item"><a href="products.php">Accessories</a></li>
+							<li class="menu-item"><a href="products.php">Promotions</a></li>
+							<li class="menu-item"><a href="products.php">PC</a></li>
+							<li class="menu-item"><a href="products.php">Playstation</a></li>
+							<li class="menu-item"><a href="products.php">Xbox</a></li>
+							<li class="menu-item"><a href="products.php">Wii</a></li>
 						</ul> <!-- .menu -->
 						<div class="search-form">
 							<label><img src="images/icon-search.png"></label>
-							<input type="text" placeholder="Buscar...">
+							<input type="text" placeholder="Search...">
 						</div> <!-- .search-form -->
 
 						<div class="mobile-navigation"></div> <!-- .mobile-navigation -->
@@ -66,44 +68,44 @@
 					<li data-bg-image="dummy/slide-1.jpg">
 						<div class="container">
 							<div class="slide-content">
-								<h2 class="slide-title">FIFA 17</h2>
-								<small class="slide-subtitle">$1.400</small>
+								<h2 class="slide-title">Kill Zone 3</h2>
+								<small class="slide-subtitle">$190.00</small>
 								
-								<p>FIFA 17 es un videojuego de fútbol desarrollado por EA Electronic publicado por EA Sports EA. y Es el 24.º de la serie y salió a la venta el 28 de septiembre del 2016 en Norteamérica y el 29 de septiembre para el resto del mundo.2 Este será el primer juego de la FIFA en la serie en emplear el motor de juego Frostbite..</p>
+								<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
 								
-								<a href="cart.html" class="button">Reservar</a>
+								<a href="#" class="button">Add to cart</a>
 							</div>
-															<!-- 16:9 aspect ratio -->
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/-3fjoe5Njpc" frameborder="0" allowfullscreen></iframe>
+
+							<img src="dummy/game-cover-1.jpg" class="slide-image">
 						</div>
 					</li>
 					<li data-bg-image="dummy/slide-2.jpg">
 						<div class="container">
 							<div class="slide-content">
 								<h2 class="slide-title">Kill Zone 3</h2>
-								<small class="slide-subtitle">$1.900</small>
+								<small class="slide-subtitle">$190.00</small>
 								
-								<p>Killzone 3 es un videojuego de acción en primera persona para PlayStation 3, desarrollado por Guerrilla Games y publicado por Sony Computer Entertainment. Es el cuarto juego de la serie Killzone.</p>
+								<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
 								
-								<a href="cart.html" class="button">Reservar</a>
+								<a href="#" class="button">Add to cart</a>
 							</div>
 
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/jqF1jlhW1fE" frameborder="0" allowfullscreen></iframe>
+							<img src="dummy/game-cover-2.jpg" class="slide-image">
 						</div>
 					</li>
 					<li data-bg-image="dummy/slide-3.jpg">
 						<div class="container">
 							<div class="slide-content">
-								<h2 class="slide-title">GTA V</h2>
-								<small class="slide-subtitle">$1.500</small>
+								<h2 class="slide-title">Kill Zone 3</h2>
+								<small class="slide-subtitle">$190.00</small>
 								
-								<p>Grand Theft Auto V (abreviado como GTA V o GTA5) es un videojuego de acción-aventura de mundo abierto desarrollado por la compañía británica Rockstar North y distribuido por Rockstar Games. Fue lanzado el 17 de septiembre de 2013 para las consolas PlayStation 3 y Xbox 360.5 Posteriormente, fue lanzado para las consolas de nueva generación PlayStation 4 y Xbox One el 18 de noviembre de 2014 y finalmente para Microsoft Windows el 14 de abril de 2015. Se trató del primer gran título en la serie Grand Theft Auto desde que se estrenara Grand Theft Auto IV en 2008, el cual estrenó la «era HD» de la mencionada serie de videojuegos.</p>
+								<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
 								
-								<a href="cart.html" class="button">Reservar</a>
-								
+								<a href="#" class="button">Add to cart</a>
 							</div>
 
-								<iframe width="560" height="315" src="https://www.youtube.com/embed/QkkoHAzjnUs" frameborder="0" allowfullscreen></iframe>						</div>
+							<img src="dummy/game-cover-3.jpg" class="slide-image">
+						</div>
 					</li>
 				</ul> <!-- .slides -->
 			</div> <!-- .home-slider -->
@@ -113,7 +115,7 @@
 					<div class="page">
 						<section>
 							<header>
-								<h2 class="section-title">Nuevos Productos</h2>
+								<h2 class="section-title">New Products</h2>
 								<a href="#" class="all">Show All</a>
 							</header>
 
@@ -121,12 +123,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-1.jpg" alt="Game 1"></a>
+											<a href="single.php"><img src="dummy/game-1.jpg" alt="Game 1"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Alpha Protocol</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -134,12 +136,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-2.jpg" alt="Game 2"></a>
+											<a href="single.php"><img src="dummy/game-2.jpg" alt="Game 2"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Grand Theft Auto V</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -147,12 +149,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-3.jpg" alt="Game 3"></a>
+											<a href="single.php"><img src="dummy/game-3.jpg" alt="Game 3"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Need for Speed rivals</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -160,12 +162,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-4.jpg" alt="Game 4"></a>
+											<a href="single.php"><img src="dummy/game-4.jpg" alt="Game 4"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Big game hunter</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -176,7 +178,7 @@
 
 						<section>
 							<header>
-								<h2 class="section-title">Promociones</h2>
+								<h2 class="section-title">promotion</h2>
 								<a href="#" class="all">Show All</a>
 							</header>
 
@@ -185,12 +187,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-5.jpg" alt="Game 1"></a>
+											<a href="single.php"><img src="dummy/game-5.jpg" alt="Game 1"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Watch Dogs</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -199,12 +201,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-6.jpg" alt="Game 2"></a>
+											<a href="single.php"><img src="dummy/game-6.jpg" alt="Game 2"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Mortal Kombat X</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -213,12 +215,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-7.jpg" alt="Game 3"></a>
+											<a href="single.php"><img src="dummy/game-7.jpg" alt="Game 3"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Metal Gear Solid V</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Add to cart</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
@@ -227,12 +229,12 @@
 								<div class="product">
 									<div class="inner-product">
 										<div class="figure-image">
-											<a href="single.html"><img src="dummy/game-8.jpg" alt="Game 4"></a>
+											<a href="single.php"><img src="dummy/game-8.jpg" alt="Game 4"></a>
 										</div>
 										<h3 class="product-title"><a href="#">Nascar '14</a></h3>
 										<small class="price">$19.00</small>
 										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
-										<a href="cart.html" class="button">Reservar</a>
+										<a href="#" class="button">Add to cart</a>
 										<a href="#" class="button muted">Read Details</a>
 									</div>
 								</div> <!-- .product -->
