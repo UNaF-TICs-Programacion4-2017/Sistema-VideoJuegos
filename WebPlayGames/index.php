66410eced3744d2a51ce5fea36b37960a8ff5434
Pablo385737142017 <pablofernandobritez2017@gmail.com>
2017-06-21 16:47:49 -0300

    prueba 2

--- WebPlayGames/index.php
+++ WebPlayGames/index.php
@@ -17,7 +17,7 @@
 		<link rel="stylesheet" href="styleeze.css">
 		
 		<!--[if lt IE 9]>
-		Hola mundo
+		Hola mundosdadsdadsasda
 		<script src="js/ie-support/html5.js"></script>
 		<script src="js/ie-support/respond.js"></script>
 		<![endif]-->
